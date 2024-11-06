82f8a4c6bc132a48c9ba3dac2ec81afe46f03462
MelikaEl <melika.melika8@gmail.com>
2024-11-06 10:27:27 +0330

    add comment for explanation of the refresh and access token

--- src/utils/apis/auth/refreshTokenApi.js
+++ src/utils/apis/auth/refreshTokenApi.js
@@ -2,7 +2,7 @@ import apiClient from "../../../constants/axios-interceptor";
 
 export const refreshTokenApi = async (data)=>{
     try{
-        return await apiClient.post("auth/refresh-token",data);
+        return await apiClient.post("auth/refresh-token",data);// in the erfresh token API, we send our access token and take the refresh token
     }
     catch (error){
         return error
