c37723e15829db3e1bb6ca4da8959a1a97c44335
MelikaEl <melika.melika8@gmail.com>
2024-11-06 10:32:28 +0330

    comments for post method of the refresh token

--- src/utils/apis/auth/refreshTokenApi.js
+++ src/utils/apis/auth/refreshTokenApi.js
@@ -2,7 +2,7 @@ import apiClient from "../../../constants/axios-interceptor";
 
 export const refreshTokenApi = async (data)=>{
     try{
-        return await apiClient.post("auth/refresh-token",data);// in the erfresh token API, we send our access token and take the refresh token
+        return await apiClient.post("auth/refresh-token",data);// in the erfresh token API, we send our access token and take the refresh token and because of sending the access token we use the post method
     }
     catch (error){
         return error
