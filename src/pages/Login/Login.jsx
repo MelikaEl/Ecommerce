1f52c670b944321ee38c66444a5b7ea86fb3cbc9
MelikaEl <melika.melika8@gmail.com>
2024-11-10 14:35:55 +0330

    add login form to the login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import LoginForm from '../../components/forms/LoginForm'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='w-[100vw] h-[100vh] bg-slate-100 flex flex-col items-center justify-center gap-4'>
+     <h1 className='font-bold text-xl'>Login</h1> 
+     <LoginForm/>
+      </div>
   )
 }
 
