da8651cae96c492f738d6d8023c0d5fd1866b9f4
MelikaEl <melika.melika8@gmail.com>
2024-11-13 11:45:17 +0330

    create dashboard route

--- src/constants/router.jsx
+++ src/constants/router.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../pages/Root";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup"
-
+import Dashboard from "../pages/Dashboard";
 
 const router = createBrowserRouter([
     {
@@ -16,6 +16,10 @@ const router = createBrowserRouter([
     {
       path: "/signup",
       element: <Signup/>,
+    },
+    {
+      path: "/dashboard",
+      element: <Dashboard/>,
     },
   ]);
 
