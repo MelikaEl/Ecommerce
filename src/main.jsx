b1740f95b97f5aa53e6b64436f4871a995e82653
MelikaEl <melika.melika8@gmail.com>
2024-11-14 13:23:39 +0330

    return the strict mode for the main file

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 // import App from "./App.jsx";
 import Providers from "./providers";
-import "./index.css"
-import 'react-toastify/dist/ReactToastify.css';
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./constants/router";
 
 createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <Providers>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
       {/* <App /> */}
     </Providers>
+  </StrictMode>
 );
