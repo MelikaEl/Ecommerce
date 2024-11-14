5cc69712d4789bc208d3950b658391627d6a86c9
MelikaEl <melika.melika8@gmail.com>
2024-11-14 13:21:31 +0330

    show alert for when already logged in in the dashboard page

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { toast } from "react-toastify";
+import { useEffect } from "react";
+import useStore from '../../store';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+  const {access_token}=useStore();
+  const navigate=useNavigate();
+  useEffect(()=>{
+    if (!access_token) {
+      toast.warn("you are not logged in!");
+      navigate("/login");
+    }
+  },[]);
+  return <div>Dashboard</div>
 }
 
 export default Dashboard
