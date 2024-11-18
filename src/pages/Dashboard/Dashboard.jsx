47e9978a442816dc96178e7097f01d2ef5e72fc4
MelikaEl <melika.melika8@gmail.com>
2024-11-18 20:39:12 +0330

    add header to the dashboard page

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import getUserInfoWithTokenApi from "../../utils/apis/users/getUserInfoWithTokenApi";
 import { useQuery } from "@tanstack/react-query";
+import Header from "../../components/common/Header/Header";
 
 import * as React from "react";
 import List from "@mui/material/List";
@@ -18,6 +19,7 @@ import Typography from "@mui/material/Typography";
 import DashboardSkeleton from "../../components/skeleton/DashboardSkeleton";
 import ErrorOnFetchApi from "../../components/common/ErrorOnFetchApi";
 import { removeCookie } from "../../utils/helpers/cookie";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 const Dashboard = () => {
   const { access_token, removeState } = useStore();
@@ -35,74 +37,79 @@ const Dashboard = () => {
   // },[]);
   console.log(data);
 
- const handleLogout = () => {
-  removeCookie("credential");
-  removeState();
-  toast.warn("logged out successfully, redirecting to login page...");
-  setTimeout(()=>navigate("/login"),1000);//navigate to the login page after 1 second
- };
-
+  const handleLogout = () => {
+    removeCookie("credential");
+    removeState();
+    toast.warn("logged out successfully, redirecting to login page...");
+    setTimeout(() => navigate("/login"), 1000); //navigate to the login page after 1 second
+  };
 
   return (
     <div>
+      <Header/>
       {access_token != null && access_token != undefined ? (
         <>
           {isPending && <DashboardSkeleton />}
-          {error && <ErrorOnFetchApi/>}
+          {error && <ErrorOnFetchApi />}
           {data && (
             <>
-            <ListItem alignItems="flex-start">
-              <div className="w-[10rem] pe-4 ">
-                <img
-                  alt="profile image"
-                  src={data?.data?.avatar}
-                  className="rounded-full"
-                />
-              </div>
+              <ListItem alignItems="flex-start">
+                <div className="w-[10rem] pe-4 ">
+                  <img
+                    alt="profile image"
+                    src={data?.data?.avatar}
+                    className="rounded-full"
+                  />
+                </div>
 
-              <ListItemText
-                primary={
-                  <p className="font-bold">{`welcome, ${data?.data?.email} !`}</p>
-                }
-                secondary={
-                  <div className="flex flex-col gap-4 mt-4">
-                    <div>
-                      <Typography
-                        component="span"
-                        variant="body2"
-                        sx={{ color: "text.primary", display: "inline" }}
-                      >
-                        name &nbsp; : &nbsp;
-                      </Typography>
-                      {data?.data?.name}
-                    </div>{" "}
-                    <div>
-                      <Typography
-                        component="span"
-                        variant="body2"
-                        sx={{ color: "text.primary", display: "inline" }}
-                      >
-                        role &nbsp; : &nbsp;
-                      </Typography>
-                      {data?.data?.role}
-                    </div>{" "}
-                    <div>
-                      <Typography
-                        component="span"
-                        variant="body2"
-                        sx={{ color: "text.primary", display: "inline" }}
-                      >
-                        password &nbsp; : &nbsp;
-                      </Typography>
-                      {data?.data?.password}
-                    </div>{" "}
-                  </div>
-                }
-              />
-             
-            </ListItem>
-             <button onClick={handleLogout} className="bg-red-500 text-slate-50 rounded-md px-4 py-2 ms-4">Logout</button>
-             </>
+                <ListItemText
+                  primary={
+                    <p className="font-bold">{`welcome, ${data?.data?.email} !`}</p>
+                  }
+                  secondary={
+                    <div className="flex flex-col gap-4 mt-4">
+                      <div>
+                        <Typography
+                          component="span"
+                          variant="body2"
+                          sx={{ color: "text.primary", display: "inline" }}
+                        >
+                          name &nbsp; : &nbsp;
+                        </Typography>
+                        {data?.data?.name}
+                      </div>{" "}
+                      <div>
+                        <Typography
+                          component="span"
+                          variant="body2"
+                          sx={{ color: "text.primary", display: "inline" }}
+                        >
+                          role &nbsp; : &nbsp;
+                        </Typography>
+                        {data?.data?.role}
+                      </div>{" "}
+                      <div>
+                        <Typography
+                          component="span"
+                          variant="body2"
+                          sx={{ color: "text.primary", display: "inline" }}
+                        >
+                          password &nbsp; : &nbsp;
+                        </Typography>
+                        {data?.data?.password}
+                      </div>{" "}
+                    </div>
+                  }
+                />
+              </ListItem>
+              <button
+                onClick={handleLogout}
+                className="bg-red-500 text-slate-50 rounded-md px-4 py-2 ms-4 flex items-center gap-2"
+              >
+                <span>Logout</span>
+                <LogoutIcon />
+              </button>
+            </>
           )}
         </>
       ) : (
