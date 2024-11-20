f69ff0829e2b927626754054cd8cda53ca49553e
MelikaEl <melika.melika8@gmail.com>
2024-11-20 14:30:25 +0330

    correct the style of the products skeleton

--- src/components/skeleton/ProductGridSkeleton/ProductGridSkeleton.jsx
+++ src/components/skeleton/ProductGridSkeleton/ProductGridSkeleton.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const ProductGridSkeleton = () => {
   return (
-    <div className="rounded-xl flex flex-col shadow-lg gap-4 items-center justify-center pb-4">
-      <div className="bg-slate-400 animate-pulse rounded-t-xl h-[8rem] w-[15rem]"></div>
+    <div className="rounded-xl flex flex-col shadow-lg gap-4 items-center justify-center pb-4 w-5/12 lg:w-3/12 my-4">
+      <div className="bg-slate-400 animate-pulse rounded-t-xl h-[15rem] w-[100%]"></div>
       <div className="bg-slate-400 animate-pulse rounded-xl w-[7rem] h-[1rem]"></div>
       <div className="bg-slate-400 animate-pulse rounded-xl w-[7rem] h-[1rem]"></div>
     </div>
