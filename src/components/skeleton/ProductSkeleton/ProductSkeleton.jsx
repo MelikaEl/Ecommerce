038a154d79d9c739cb96bd3a0a12cdaff3cc9ca3
MelikaEl <melika.melika8@gmail.com>
2024-11-24 09:24:50 +0330

    change the style of  product skeleton

--- src/components/skeleton/ProductSkeleton/ProductSkeleton.jsx
+++ src/components/skeleton/ProductSkeleton/ProductSkeleton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ProductSkeleton = () => {
   return (
     <>
-      <div className="bg-slate-400 animate-pulse w-[10rem] h-[10rem] rounded-xl"></div>
+      <div className="bg-slate-400 animate-pulse w-[15rem] h-[15rem] rounded-xl"></div>
       <div className="flex gap-2 flex-wrap">
         <div className="bg-slate-400 animate-pulse w-[5rem] h-[5rem] rounded-xl"></div>
         <div className="bg-slate-400 animate-pulse w-[5rem] h-[5rem] rounded-xl"></div>
