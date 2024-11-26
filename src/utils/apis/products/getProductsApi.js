09076154247ff35756fbc11a70063cb27d07ec4d
MelikaEl <melika.melika8@gmail.com>
2024-11-26 14:02:13 +0330

    get products API with offset and limit

--- src/utils/apis/products/getProductsApi.js
+++ src/utils/apis/products/getProductsApi.js
@@ -1,11 +1,13 @@
 import apiClient from "../../../constants/axios-interceptor";
 
-export const getProductsApi=async()=>{
-    try{
-        return await apiClient.get("/products");
-    }
-    catch (error){
-        return error
-    }
-}
+export const getProductsApi = async (offset = null, limit = null) => {
+  try {
+    return await apiClient.get(
+      `/products?${
+        offset != null && limit != null && `offset=${offset}&limit=${limit}`}`
+    ); // (page -1) * limit    : if number of the page is 1 and want to find the offset and limit, 1-1=0   0*10=0  offset=0. If if number of the page is 2 and want to find the offset and limit, 2-1=1   1*10=10 offset=10 and so on...
+  } catch (error) {
+    return error;
+  }
+};
 export default getProductsApi;
